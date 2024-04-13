ebe05abeb8eb8fefe8d77d66c4efb465facf8d6c
Vedant <76871277+theofficialvedantjoshi@users.noreply.github.com>
2024-04-14 03:46:21 +0530

    created quiz front end

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
+from flask import (
+    Flask,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    send_file,
+)
+import pdfplumber
+from werkzeug.utils import secure_filename
+import os
+from src.quiz_generator import gen_quiz
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +20,40 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/quiz", methods=["GET", "POST"])
+def quiz():
+    if request.method == "POST":
+        pdf = request.files["file"]
+        # parsefile
+        path = "files/" + secure_filename(pdf.filename)
+        pdf.save(path)
+        pdf = pdfplumber.open(path)
+        raw_text = ""
+        for page in pdf.pages:
+            raw_text += page.extract_text()
+
+        topic = request.form["topic"]
+        number_of_questions = request.form["number_of_q"]
+        question_type = request.form["type"]
+        data = gen_quiz(raw_text, topic, number_of_questions, question_type)
+        print(data)
+        questions = "test"
+        answers = "test"
+        return render_template(
+            "answers.html",
+            questions=questions,
+            answers=answers,
+        )
+    question_types = [
+        "mcq",
+        "True/False",
+        "Fill in the Blank",
+        "Short Answer",
+        "Long Answer",
+    ]
+    return render_template("quiz.html", question_types=question_types)
 
 
 if __name__ == "__main__":
